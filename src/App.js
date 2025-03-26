7a196d9927211edefbe4f86ee854121c898edde1
arjun2k01 <122463451+arjun2k01@users.noreply.github.com>
2025-03-26 16:50:23 +0530

    Added the authForm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Admin from "./pages/Admin";
+import AuthForm from "./components/auth";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/admin" element={<Admin />} />
+        <Route path="/Auth" element={< AuthForm/>} />
       </Routes>
     </Router>
   );
