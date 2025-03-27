6f98b026b5b0ce932f6e0d4c62dfbc8081275149
PAPPU KEERTHIKA <nagadurga743@gmai.com>
2025-03-27 21:07:11 +0530

    Updated sidebar icon size in collapsed mode

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { FaHome, FaUser, FaUsers, FaClock, FaFileAlt, FaChartBar, FaCog, FaBars } from "react-icons/fa"; // Added FaUsers
 import "./Sidebar.css";
 
 const Sidebar = () => {
@@ -9,16 +10,52 @@ const Sidebar = () => {
     <>
       <div className={`sidebar ${isOpen ? "open" : "collapsed"}`}>
         <button className="toggle-btn" onClick={() => setIsOpen(!isOpen)}>
-          ☰
+          <FaBars />
         </button>
         <ul>
-          <li><Link to="/admin">Admin</Link></li>
-          <li><Link to="/dashboard">Dashboard</Link></li>
-          <li><Link to="/employee">Employee</Link></li>
-          <li><Link to="/timesheet">Timesheet</Link></li>
-          <li><Link to="/timeclaim">Time Claim</Link></li>
-          <li><Link to="/reports">Reports</Link></li>
-          <li><Link to="/settings">Settings</Link></li>
+          <li>
+            <Link to="/admin">
+              <FaUser className="icon" />
+              <span className="text">Admin</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/dashboard">
+              <FaHome className="icon" />
+              <span className="text">Dashboard</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/employee">
+              <FaUsers className="icon" /> {/* Changed to FaUsers */}
+              <span className="text">Employee</span>
+            </Link>
+          </li>
+          {/* Rest unchanged */}
+          <li>
+            <Link to="/timesheet">
+              <FaClock className="icon" />
+              <span className="text">Timesheet</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/timeclaim">
+              <FaFileAlt className="icon" />
+              <span className="text">Time Claim</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/reports">
+              <FaChartBar className="icon" />
+              <span className="text">Reports</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/settings">
+              <FaCog className="icon" />
+              <span className="text">Settings</span>
+            </Link>
+          </li>
         </ul>
       </div>
       <div className={`content ${isOpen ? "shifted" : ""}`}>
